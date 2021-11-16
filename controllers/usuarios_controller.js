3a6e38eb4121ea5512f7b043db8d92a39b5178d3
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-16 20:51:49 -0300

    Params

--- controllers/usuarios_controller.js
+++ controllers/usuarios_controller.js
@@ -1,9 +1,12 @@
-const { response } = require('express') // Opcional por la falta de tipado estricto de JS
+const { response, request } = require('express') // Opcional por la falta de tipado estricto de JS
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = (response = response)) => {
+  const { a, b, nombre = 'Sin nombre' } = req.query
   res.json({
-    // ok: true, Resp.body(statusCode = 200) => peticion ok
     msg: 'Get api -----> Controlador',
+    a,
+    b,
+    nombre,
   })
 }
 
@@ -18,8 +21,10 @@ const usuariosPost = (req, res) => {
 }
 
 const usuariosPut = (req, res) => {
+  const { id } = req.params
   res.json({
     msg: 'Put api -----> Controlador',
+    id,
   })
 }
 
--- routes/usuarios_route.js
+++ routes/usuarios_route.js
@@ -13,7 +13,7 @@ const router = Router()
 
 router.get('/', usuariosGet)
 
-router.put('/', usuariosPut)
+router.put('/:id', usuariosPut)
 
 router.post('/', usuariosPost)
 
