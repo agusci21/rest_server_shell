9d74e0f8b199dff2fecf55e79dcc0aa35a93e5d8
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-20 12:57:12 -0300

    contraseña encriptada

--- controllers/usuarios_controller.js
+++ controllers/usuarios_controller.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express') // Opcional por la falta de tipado estricto de JS
+const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario')
 
 const usuariosGet = (req = request, res = (response = response)) => {
@@ -12,8 +13,12 @@ const usuariosGet = (req = request, res = (response = response)) => {
 }
 
 const usuariosPost = async (req, res) => {
-  const body = req.body
-  const usuario = new Usuario(body)
+  const { nombre, correo, password, rol } = req.body
+  const usuario = new Usuario({ nombre, correo, password, rol })
+
+  const salt = bcryptjs.genSaltSync()
+
+  usuario.password = bcryptjs.hashSync(password, salt)
 
   await usuario.save()
 
