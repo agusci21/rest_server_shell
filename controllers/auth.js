5c3b923aee5c7cf3920a9961caddbfdfc5b41298
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-21 11:29:15 -0300

    Validar datos del login menos JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,44 @@
-const { response } = require("express");
+const { response } = require('express')
+const Usuario = require('../models/usuario')
+const bcryptjs= require('bcryptjs')
 
-const login = (req, res = response) => {
-res.json({
-    msg: 'Login okay'
-})
+const login = async (req, res = response) => {
+  const { correo, password } = req.body
+
+  try {
+    //Email existente
+    const usuario = await Usuario.findOne({correo})
+    if(!usuario){
+        return res.status(400).json({
+            msg:'Usuario o contraseña invalidos - correo'
+        })
+    }
+    //Usuario activo
+    if(!usuario.estado){
+        return res.status(400).json({
+            msg : 'Usuario o contraseña invalidos - estado: false'
+        })
+    }
+    //verificar contraseña
+    const validPassword = bcryptjs.compareSync(password, usuario.password)
+    if(!validPassword){
+        return res.status(400).json({
+            msg:'Usuario o contraseña invalidos - contraseña'
+        })
+    }
+    //generar JWT
+
+    return res.json({
+      msg: 'Login okay',
+    })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).json({
+      msg: 'Algo Salio mal',
+    })
+  }
 }
 
 module.exports = {
-    login
+  login,
 }
