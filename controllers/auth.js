c7d8d784922385877bf1448ba15355f2b94bdefc
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-22 10:13:12 -0300

    generacion del JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,40 @@
 const { response } = require('express')
 const Usuario = require('../models/usuario')
-const bcryptjs= require('bcryptjs')
+const bcryptjs = require('bcryptjs')
+const { generarJWT } = require('../helpers/generar-jwt')
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body
 
   try {
     //Email existente
-    const usuario = await Usuario.findOne({correo})
-    if(!usuario){
-        return res.status(400).json({
-            msg:'Usuario o contraseña invalidos - correo'
-        })
+    const usuario = await Usuario.findOne({ correo })
+    if (!usuario) {
+      return res.status(400).json({
+        msg: 'Usuario o contraseña invalidos - correo',
+      })
     }
     //Usuario activo
-    if(!usuario.estado){
-        return res.status(400).json({
-            msg : 'Usuario o contraseña invalidos - estado: false'
-        })
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: 'Usuario o contraseña invalidos - estado: false',
+      })
     }
     //verificar contraseña
     const validPassword = bcryptjs.compareSync(password, usuario.password)
-    if(!validPassword){
-        return res.status(400).json({
-            msg:'Usuario o contraseña invalidos - contraseña'
-        })
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: 'Usuario o contraseña invalidos - contraseña',
+      })
     }
     //generar JWT
 
+    const token = await generarJWT(usuario.id)
+
     return res.json({
       msg: 'Login okay',
+      usuario,
+      token,
     })
   } catch (e) {
     console.log(e)
