c60a4394510b281f59c876b7050d39ca6699046d
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-22 10:18:33 -0300

    uid = id visual

--- models/usuario.js
+++ models/usuario.js
@@ -1,43 +1,41 @@
-
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose')
 
 const UsuarioSchema = Schema({
-    nombre: {
-        type: String,
-        required: [true, 'El nombre es obligatorio']
-    },
-    correo: {
-        type: String,
-        required: [true, 'El correo es obligatorio'],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, 'La contraseña es obligatoria'],
-    },
-    img: {
-        type: String,
-    },
-    rol: {
-        type: String,
-        required: true,
-        emun: ['ADMIN_ROLE', 'USER_ROLE']
-    },
-    estado: {
-        type: Boolean,
-        default: true
-    },
-    google: {
-        type: Boolean,
-        default: false
-    },
-});
+  nombre: {
+    type: String,
+    required: [true, 'El nombre es obligatorio'],
+  },
+  correo: {
+    type: String,
+    required: [true, 'El correo es obligatorio'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria'],
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    emun: ['ADMIN_ROLE', 'USER_ROLE'],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+})
 
-
-
-UsuarioSchema.methods.toJSON = function() {
-    const { __v, password, ...usuario  } = this.toObject();
-    return usuario;
+UsuarioSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...usuario } = this.toObject() // Esto extrae argumentos y los quita del JSON
+  usuario.uid = _id
+  return usuario
 }
 
-module.exports = model( 'Usuario', UsuarioSchema );
+module.exports = model('Usuario', UsuarioSchema)
