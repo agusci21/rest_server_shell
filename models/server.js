a485695df7640daae6a1ec7ee1ff442a5fa2863d
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-23 21:05:48 -0300

    Creacion de los endpoints

--- models/server.js
+++ models/server.js
@@ -1,60 +1,55 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-const { dbConnection } = require('../database/config');
+const { dbConnection } = require('../database/config')
 
 class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT
 
-    constructor() {
-        this.app  = express();
-        this.port = process.env.PORT;
-
-        this.usuariosPath = '/api/usuarios';
-        this.authPath     = '/api/auth';
-
-        // Conectar a base de datos
-        this.conectarDB();
-
-        // Middlewares
-        this.middlewares();
-
-        // Rutas de mi aplicación
-        this.routes();
+    this.paths = {
+      auth: '/api/usuarios',
+      categorias: '/api/categorias',
+      usuarios: '/api/auth',
     }
 
-    async conectarDB() {
-        await dbConnection();
-    }
+    // Conectar a base de datos
+    this.conectarDB()
 
+    // Middlewares
+    this.middlewares()
 
-    middlewares() {
+    // Rutas de mi aplicación
+    this.routes()
+  }
 
-        // CORS
-        this.app.use( cors() );
+  async conectarDB() {
+    await dbConnection()
+  }
 
-        // Lectura y parseo del body
-        this.app.use( express.json() );
+  middlewares() {
+    // CORS
+    this.app.use(cors())
 
-        // Directorio Público
-        this.app.use( express.static('public') );
+    // Lectura y parseo del body
+    this.app.use(express.json())
 
-    }
+    // Directorio Público
+    this.app.use(express.static('public'))
+  }
 
-    routes() {
-        
-        this.app.use( this.authPath, require('../routes/auth'));
-        this.app.use( this.usuariosPath, require('../routes/usuarios'));
-    }
+  routes() {
+    this.app.use(this.paths.auth, require('../routes/auth'))
+    this.app.use(this.paths.usuarios, require('../routes/usuarios'))
+    this.app.use(this.paths.categorias, require('../routes/categorias'))
+  }
 
-    listen() {
-        this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port );
-        });
-    }
-
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Servidor corriendo en puerto', this.port)
+    })
+  }
 }
 
-
-
-
-module.exports = Server;
+module.exports = Server
---
+++ routes/categorias.js
@@ -0,0 +1,42 @@
+const { Router } = require('express')
+const { check } = require('express-validator')
+
+const { validarCampos } = require('../middlewares/validar-campos')
+
+const router = Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    msg: 'Get',
+  })
+})
+
+router.get('/:id', (req, res) => {
+    res.status(200).json({
+      msg: 'Get',
+    })
+  })
+
+  router.post('/', (req, res) => {
+    res.status(200).json({
+      msg: 'post',
+    })
+  })
+
+  router.put('/:id', (req, res) => {
+    res.status(200).json({
+      msg: 'put',
+    })
+  })
+
+  router.delete('/:id', (req, res) => {
+    res.status(200).json({
+      msg: 'delete',
+    })
+  })
+
+
+
+
+
+module.exports = router
