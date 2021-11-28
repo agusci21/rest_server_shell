4fdd3509d02ff4c7df4346fb189ee1e941a8d492
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-11-28 11:28:00 -0300

    Subir archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,8 +1,27 @@
+const path = require('path')
+
 const { response } = require('express')
 
 const cargarArchivo = (req, res = response) => {
-  res.json({
-    msg: 'Cargar Archivo',
+  let sampleFile
+
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).json({
+      msg: 'No se seleccionaros archivos',
+    })
+    return
+  }
+
+  const { archivo } = req.files
+
+  const uploadPath = path.join(__dirname, '../uploads/' + archivo.name)
+
+  archivo.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({err})
+    }
+
+    res.json({msg: 'Archivo subido a: ' + uploadPath})
   })
 }
 
--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const { dbConnection } = require('../database/config')
+const fileUpload = require('express-fileupload')
 
 class Server {
   constructor() {
@@ -40,6 +41,12 @@ class Server {
 
     // Directorio Público
     this.app.use(express.static('public'))
+
+    //Subir archivos
+    this.app.use(fileUpload({
+      useTempFiles: true,
+      tempFileDir: '/tmp/'
+    }))
   }
 
   routes() {
